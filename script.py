bb99ca0005f1bf6df451cceaca8ea1396a28b113
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-04-26 19:25:01 +0300

    add selenium integration and interaction with AC
    In this commit I've added the first part of the Selenium integration,
    the script logs in into Active campaing, specifically it logs in and
    goes to the following page: https://tstprep.activehosted.com/app/automations
    It then uses the google sheets api to read data from a  google spreadsheet.

--- script.py
+++ script.py
@@ -1,6 +1,40 @@
+# Python Selenium - imports
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Python GoogleSheetsAPI-imports
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
+
+
+# Python Selenium - code
+browser1 = webdriver.Chrome()
+browser1.get('https://tstprep.activehosted.com/app/automations')
+
+element_usr = WebDriverWait(browser1, 5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "input#user"))
+)
+element_usr.send_keys('josh@tstprep.com')
+
+
+element_pas = WebDriverWait(browser1, 5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "input#pass"))
+)
+element_pas.send_keys('Z8kkh31SEVHm')
+
+element_pas = WebDriverWait(browser1, 5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "input[value='Login']"))
+)
+element_pas.click()
+
+
+
+
+
+# Python GoogleSheetsAPI - code
 SERVICE_ACCOUNT_FILE = 'keys.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
