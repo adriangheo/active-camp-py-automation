88334e73c799047a7b953f49a4907c0b0bacff49
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-04-27 23:44:37 +0300

    no idea

--- pages/automations_page.py
+++ pages/automations_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from .base_element import BaseElement
 from .base_elements import BaseElements
 from .base_page import BasePage
-from .locator import Locator
+
 
 class AutomationsPage(BasePage):
     url = 'https://tstprep.activehosted.com'
@@ -11,33 +11,48 @@ class AutomationsPage(BasePage):
     @property
     # def courses_for_duolingo(self):
     def username_field(self):
-        lctr = Locator(By.CSS_SELECTOR, "input#user")
-        return BaseElement(driver = self.driver, locator=lctr)
+        locator = (By.CSS_SELECTOR, "input#user")
+        return BaseElement(
+            driver = self.driver, 
+            by = locator[0],
+            value = locator[1])
 
     @property
     # def private_lessons_for_duolingo(self):
     def password_field(self):
-        lctr = Locator(By.CSS_SELECTOR, "input#pass")
-        return BaseElement(driver = self.driver, locator=lctr)
+        locator = (By.CSS_SELECTOR, "input#pass")
+        return BaseElement(
+            driver = self.driver, 
+            by = locator[0],
+            value = locator[1])
 
     @property
     # def score_evaluation_for_duolingo(self):
     def login_btn(self):
-        lctr = Locator(By.CSS_SELECTOR, "input[value='Login']")
-        return BaseElement(driver = self.driver, locator=lctr)
+        locator = (By.CSS_SELECTOR, "input[value='Login']")
+        return BaseElement(driver = self.driver, 
+            by = locator[0],
+            value = locator[1])
 
     @property
     def list_of_automations(self):
-        lctr = Locator(By.CSS_SELECTOR, ".inner.flex.flex-column.p-4")
-        return BaseElements(driver = self.driver, locator=lctr)
+        locator = (By.CSS_SELECTOR, ".inner.flex.flex-column.p-4")
+        return BaseElements(
+            driver = self.driver, 
+            by = locator[0],
+            value = locator[1]
+        )
 
 
 
 
     @property
     def list_of_divs(self):
-        lctr = Locator(By.CSS_SELECTOR, ".inner.flex.flex-column.p-4")
-        return BaseElements(driver = self.driver, locator=lctr)
+        locator = (By.CSS_SELECTOR, ".inner.flex.flex-column.p-4")
+        return BaseElements(
+            driver = self.driver, 
+            by = locator[0],
+            value = locator[1])
 
 
 
