ad6da3dc8e16080cc3c18825b421b3b61c012036
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-04-28 18:21:42 +0300

    call a function from a custom class (google_api.py)

---
+++ apis/google_api.py
@@ -0,0 +1,40 @@
+# Python GoogleSheetsAPI-imports
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+
+
+class GoogleApi:
+
+    SERVICE_ACCOUNT_FILE = 'keys.json'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+    my_credentials = None
+    my_credentials = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    # The ID spreadsheet.
+    SAMPLE_SPREADSHEET_ID = '1dAFFThOXIuM-YKtKMEzSgHaD0HPpFLsUamiMZhlrOOk'
+
+    service = build('sheets', 'v4', credentials=my_credentials)
+
+    # Call the Sheets API
+    mydata = [["8/1/2020","Joe3","MidWest","IL","New Brand",563.65,342.45]]
+
+
+    def appendToActCampAutomationsSheet(self, data):
+        request = self.service.spreadsheets().values().append(
+                spreadsheetId=self.SAMPLE_SPREADSHEET_ID, 
+                range="ActCampAutomationsSheet!A2", # where to start from in searching for the first empty row
+                valueInputOption="USER_ENTERED", #how the input data should be interepreted. Either a)RAW (will not be parsed) or b)USER_ENTERED (ie. strings may be converted to nubmers, dates, etc)
+                insertDataOption="INSERT_ROWS", #how the input data should be inserted. Either a) OVERWRITE or b)INSERT_ROWS
+                body={"values":data} #
+        )
+
+        request.execute()
+        return None
+
+
+
+
+
--- script.py
+++ script.py
@@ -8,89 +8,52 @@ from selenium.webdriver.support import expected_conditions as EC
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
+from pages.automations_page import AutomationsPage
+from apis.google_api import GoogleApi
+
+google_api = GoogleApi()
+
+# Python Selenium - code
+browser1 = webdriver.Chrome()
+
+# browser1.get('https://tstprep.activehosted.com/app/automations?limit=100&offset=100&page=2')
+browser1.get('https://tstprep.activehosted.com/app/automations?limit=100')
 
 
-# # Python Selenium - code
-# browser1 = webdriver.Chrome()
-
-# # browser1.get('https://tstprep.activehosted.com/app/automations?limit=100&offset=100&page=2')
-# browser1.get('https://tstprep.activehosted.com/app/automations?limit=100')
-
-
-# element_usr = WebDriverWait(browser1, 5).until(
-#     EC.presence_of_element_located((By.CSS_SELECTOR, "input#user"))
-# )
-# element_usr.send_keys('josh@tstprep.com')
+element_usr = WebDriverWait(browser1, 5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "input#user"))
+)
+element_usr.send_keys('josh@tstprep.com')
 
 
-# element_pas = WebDriverWait(browser1, 5).until(
-#     EC.presence_of_element_located((By.CSS_SELECTOR, "input#pass"))
-# )
-# element_pas.send_keys('Z8kkh31SEVHm')
-
-
-
-# element_pas = WebDriverWait(browser1, 5).until(
-#     EC.presence_of_element_located((By.CSS_SELECTOR, "input[value='Login']"))
-# )
-# element_pas.click()
-
-
-# automations  = WebDriverWait(browser1, 5).until(
-#     EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.automations_index_automation-list_list-row .automation_title > a'))
-# )
-
-# list_of_automations_data = []
-
-# for elmnt in automations :  
-#     automation_data = []
-#     automation_data.append(elmnt.get_attribute("href"))
-#     automation_data.append(elmnt.text)
-#     list_of_automations_data.append(automation_data)
+element_pas = WebDriverWait(browser1, 5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "input#pass"))
+)
+element_pas.send_keys('Z8kkh31SEVHm')
 
 
 
-
-# 
-# 
-# 
-
-SERVICE_ACCOUNT_FILE = 'keys.json'
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-my_credentials = None
-my_credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-# The ID spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1dAFFThOXIuM-YKtKMEzSgHaD0HPpFLsUamiMZhlrOOk'
-
-service = build('sheets', 'v4', credentials=my_credentials)
-
-# Call the Sheets API
-mydata = [["8/1/2020","Joe3","MidWest","IL","New Brand",563.65,342.45]]
+element_pas = WebDriverWait(browser1, 5).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "input[value='Login']"))
+)
+element_pas.click()
 
 
-def append_sheet(data):
-    request = service.spreadsheets().values().append(
-            spreadsheetId=SAMPLE_SPREADSHEET_ID, 
-            range="AC-Automations-List!A2", # where to start from in searching for the first empty row
-            valueInputOption="USER_ENTERED", #how the input data should be interepreted. Either a)RAW (will not be parsed) or b)USER_ENTERED (ie. strings may be converted to nubmers, dates, etc)
-            insertDataOption="INSERT_ROWS", #how the input data should be inserted. Either a) OVERWRITE or b)INSERT_ROWS
-            body={"values":data} #
-    )
+automations  = WebDriverWait(browser1, 5).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.automations_index_automation-list_list-row .automation_title > a'))
+)
 
-    request.execute()
-    return None
+list_of_automations_data = []
 
-append_sheet(mydata)
+for elmnt in automations :  
+    automation_data = []
+    automation_data.append(elmnt.get_attribute("href"))
+    automation_data.append(elmnt.text)
+    list_of_automations_data.append(automation_data)
 
 
 
-request = service.spreadsheets().values().append(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID, 
-        range="AC-Automations-List!A2", # where to start from in searching for the first empty row
-        valueInputOption="USER_ENTERED", #how the input data should be interepreted. Either a)RAW (will not be parsed) or b)USER_ENTERED (ie. strings may be converted to nubmers, dates, etc)
-        insertDataOption="INSERT_ROWS", #how the input data should be inserted. Either a) OVERWRITE or b)INSERT_ROWS
-        body={"values":list_of_automations_data} #
-).execute()
+
+google_api.appendToActCampAutomationsSheet(list_of_automations_data)
+
+
