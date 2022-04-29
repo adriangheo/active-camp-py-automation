7be4f224cba06257218600cffb46ee4c73da35a8
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-04-29 20:10:40 +0300

    iterating through range, that will eventually be the target google sheet range

--- script.py
+++ script.py
@@ -63,18 +63,20 @@ href_cells_list = []
 target_columns = ['D', 'E', 'F', 'G', 'H', 'I']
 target_lines = [*range(2, 1301, 1)]
 
-# for line_idx, line_elmnt in enumerate(target_lines):
-#     intermediary_list = []
-#     href_cells_list.append(intermediary_list)
-#     for col_idx, col_elmnt in enumerate(target_columns):
-#         # print(str(target_lines[line_idx]) + target_columns[col_idx])
-#         print(href_cells_list)
-
-
 for line_idx, line_elmnt in enumerate(target_lines):
+    intermediary_list = []
+    href_cells_list.append(intermediary_list)
     for col_idx, col_elmnt in enumerate(target_columns):
-        print(target_columns[col_idx] + str(target_lines[line_idx]))
-    print("---")
+        # print(str(target_lines[line_idx]) + target_columns[col_idx])
+        intermediary_list.append( target_columns[col_idx] + str(target_lines[line_idx]) )
+
+print(href_cells_list)
+
+
+# for line_idx, line_elmnt in enumerate(target_lines):
+#     for col_idx, col_elmnt in enumerate(target_columns):
+#         print(target_columns[col_idx] + str(target_lines[line_idx]))
+#     print("---")
 
 # #first run
 # print(result)
