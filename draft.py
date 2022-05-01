8dd2ea2b5c285a2020d6d0a209ee213db0eba6e0
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-05-01 17:10:26 +0300

    working on traversing urls

--- draft.py
+++ draft.py
@@ -1,20 +1,58 @@
+# Python Selenium - imports
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Python GoogleSheetsAPI-imports
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+from pages.automations_page import AutomationsPage
+from pages.login_page import LoginPage
+from apis.google_api import GoogleApi
+
 import time 
 
 
+# # Python Selenium - code
+# browser = webdriver.Chrome()
 
-def wait_half_a_sec():
-    time.sleep(0.5)
-    print("half a sec")
+# login_page = LoginPage(driver=browser)
+
+# login_page.go()
+
+# login_page.username_field.input_text('marian@dacianempire.com')
+# login_page.password_field.input_text('daTIVoINgerymPolIzAR')
+# login_page.login_btn.click()
+
+# browser.get('https://thethirdwave.activehosted.com/report/#/campaign/1/overview')
 
 
-def wait_entire_sec():
-    time.sleep(1)
-    print("entire sec")
 
 
-wait_entire_sec()
-wait_entire_sec()
-wait_entire_sec()
-wait_half_a_sec()
-wait_half_a_sec()
-wait_half_a_sec()
+
+# # if(browser.current_url == 'https://thethirdwave.activehosted.com/report/#/campaign'):
+
+# currentUrl = browser.current_url
+# wait = WebDriverWait(browser, 4)
+
+
+# flagUrlNonExistant = False
+# def waitForUrlChange():
+#     global currentUrl
+#     wait.until(EC.url_changes(currentUrl)) 
+#     if(currentUrl == "https://thethirdwave.activehosted.com/report/#/campaign"):
+#         flagUrlNonExistant = True
+    
+
+# waitForUrlChange()
+
+
+my_range = range(1, 20, 1)
+
+for n in my_range:
+    print("https://thethirdwave.activehosted.com/report/#/campaign/"+ str(n) +"/overview")
+    
+
+
